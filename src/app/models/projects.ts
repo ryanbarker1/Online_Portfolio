ee2603687e204e881536ca2783e80b6e70f23e46
ryanbarker1 <ryanbarker2@mail.weber.edu>
2019-04-01 17:56:47 -0600

    4/1 changes

--- src/app/models/projects.ts
+++ src/app/models/projects.ts
@@ -6,35 +6,42 @@ export const MY_PROJECTS : Project[] = [
         URL: "http://goldprogram.wpengine.com/",
         title: "The Gold Program",
         description: "This site was done with WordPress. The Gold Program is part of the company WEX. This site is where existing members and those who are looking to find ways to save more money on their tire purchases. This is desingned with custom CSS, Javascript and PHP. The login portion is used with .NET",
-        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/port3.JPG"
+        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/Capture08.jpg"
     },
     {
         id: 105,
         URL: "https://new.ftsplus.com/",
         title: "FTS+",
         description: "This site is going to be completed soon. Its is done through DNN9 (an online editor) just like WordPress. This is where members will go to login to view their savings and sign up for more. Using a form that goes to Salesforce.",
-        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/port6.JPG"
+        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/Capture06.jpg"
     },
     {
         id: 106,
         URL: "https://page-preview.instapage.com/preview/6be828ba6623024d24d0ca73e94855fc54d9987e155c219bf77922b81c190c92?displayMode=0&version=2&featureMode=2&variation=A&hotspotId=null",
         title: "Fleet One EDGE landing page",
         description: "This site is completed by using an online text editor Instapages. It is a site that you can quickly create a page. This page is one of many landing pages that a user can fill out a form to recieve more savings.",
-        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/Capture.JPG"
+        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/Capture01.jpg"
     },
     {
         id: 107,
         URL: "https://page-preview.instapage.com/preview/b7ebc71a467963d26aea824d63f44909d8028e42e27033db60597573ab8c2bc6?displayMode=0&version=2&featureMode=2&variation=A&hotspotId=null",
         title: "The Gold Program testimony page",
         description: "This site is completed by using an online text editor Instapages. It is a site that you can quickly create a page. This page is where a member from The Gold Program can fill out a testimonial form that will be used on the main site.",
-        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/Capture1.JPG"
+        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/Capture02.jpg"
     },
     {
         id: 108,
         URL: "https://benefits.wexedge.com/fuelbetafaq",
         title: "WEX EDGE FAQ",
         description: "This site is completed by using an online text editor Instapages. It is a site that you can quickly create a page. This page is one of many landing pages. This site is where if a user has questions about a certain subject they can be able to search through and find what they are looking for.",
-        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/Capture2.JPG"
+        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/Capture03.jpg"
+    },
+    {
+        id: 104,
+        URL: "https://joejanson.netlify.com/",
+        title: "Joe Janson",
+        description: "This project that I have been working on with a client. It is not completed yet. This is going to be done through WordPress except for the landing page which is done using HTML, Bootstrap and CSS3 animations. With this site is going to be used for having visitors to the site be able to purchase the clients books.",
+        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/Capture07.jpg"
     },
 
     {
@@ -42,20 +49,34 @@ export const MY_PROJECTS : Project[] = [
         URL: "https://keen-almeida-f9cbb4.netlify.com/#/",
         title: "Sunridge",
         description: "This site was built with Vue.js and bootstrap. This was built for a client for a school project who wanted to update an old site that is still in use. The old site was made in the 90s so it made better since to build a new one up to date.",
-        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/port.jpg"
+        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/Capture04.jpg"
     },
     {
         id: 103,
         URL: "https://ryanbarker1.github.io/web2500/index.html",
         title: "Web Project",
         description: "This was done in bootstrap and it was a school final project for class.",
-        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/port2.JPG"
+        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/Capture05.jpg"
     },
     {
-        id: 104,
-        URL: "https://ryanbarker1.github.io/client2/",
+        id: 109,
+        URL: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/EDGE_SprintSavings.jpg",
         title: "Web Project",
-        description: "This was a project for class that I had to create a clients website. Was done with CSS3, Bootstrap, and PHP.",
-        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/port7.JPG"
+        description: "This was designed in InDesign. It is a flier that is sent out to recepients via emails.",
+        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/Capture09.jpg"
+    },
+    {
+        id: 110,
+        URL: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/gold.jpg",
+        title: "Web Project",
+        description: "This was designed in InDesign. It is a flier that is sent out to recepients via emails.",
+        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/Capture10.jpg"
+    },
+    {
+        id: 111,
+        URL: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/GoldOverview_EDGE.jpg",
+        title: "Web Project",
+        description: "This was designed in InDesign. It is a flier that is sent out to recepients via emails.",
+        image: "https://raw.githubusercontent.com/ryanbarker1/ryanbarker1.github.io/master/portfolio/Capture11.jpg"
     }
 ]
